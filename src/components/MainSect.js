c0431af636fd6b2691a6e21ae3bcc6c04363e4f7
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2023-04-15 18:59:53 -0500

    Setup mainsection columns

--- src/components/MainSect.js
+++ src/components/MainSect.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import {Container, Row , Col } from 'react-bootstrap';
 
 const MainSect = () => {
   return (
     <section id="main-section">
-     
-        <h1>Hello World!</h1>
-      
+      <Container>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }} id="title-card">
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
+              </Card.Text>
+            </Card>
+          </Col>
+          
+        </Row>
+
+      </Container>
     </section>
   );
 };
