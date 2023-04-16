55be20155ff79d31b2dbf93406b3009372d6796c
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2023-04-16 17:24:16 -0500

    create projects section row and columns

---
+++ src/components/MyProjects.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MyProjects = () => {
+  return (
+    <section id="projects-section">
+      <Container id="projects-container">
+        <Row>
+          {/* Project #1 */}
+          <Col></Col>
+          {/* Project #2 */}
+          <Col></Col>
+          {/* Project #3 */}
+          <Col></Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default MyProjects;
--- src/components/NavDirect.js
+++ src/components/NavDirect.js
@@ -1,23 +1,25 @@
 import React from "react";
-import {Navbar, Container, Nav, Button} from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 const NavDirect = () => {
-  return(
-  <div>
-    <Navbar id= "nav-menu" sticky="top">
+  return (
+    <div>
+      <Navbar id="nav-menu" sticky="top">
         <Container>
-          <Navbar.Brand id= "nav-brand" href="#home">TF</Navbar.Brand>
-          <Nav className= "ml-auto"/>
+          <Navbar.Brand id="nav-brand" href="#home">
+            TF
+          </Navbar.Brand>
+          <Nav className="ml-auto" />
           <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">My Work</Nav.Link>
-            <Nav.Link href="#link"><Button variant="info">Contact Me</Button></Nav.Link>
+          <Nav.Link href="#link">About</Nav.Link>
+          <Nav.Link href="#link">My Work</Nav.Link>
+          <Nav.Link href="#link">
+            <Button variant="info">Contact Me</Button>
+          </Nav.Link>
         </Container>
       </Navbar>
-     
-        
-        </div>
-  )
+    </div>
+  );
 };
 
 export default NavDirect;
