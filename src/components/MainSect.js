2c27692d587a5934146fdd9c761e3164e68b4d38
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2023-04-16 16:27:31 -0500

    transparent background for navbar

--- src/components/MainSect.js
+++ src/components/MainSect.js
@@ -5,17 +5,15 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 const MainSect = () => {
   return (
     <section id="main-section">
-      <Container>
+      <Container id="main-container">
         <Row>
           <Col>
-            <Card style={{ width: "18rem" }} id="title-card">
+            <Card style={{ width: "32rem" }} id="title-card">
               <Card.Text>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                nisi ut aliquip ex ea commodo consequat. 
               </Card.Text>
             </Card>
           </Col>
